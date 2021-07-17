30a4ad8aa4a3dcd8c5e0cb1bc9f154e7ccac363f
Golden <1783327105@qq.com>
2021-07-17 11:50:02 +0800

    测试欢迎页面调用

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @description: TODO
+ * @description: 测试欢迎页面调用
  * @author: Golden
  * @date: 2021/7/13 16:43
  */
